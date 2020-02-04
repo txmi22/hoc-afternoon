e3c592d66967c397b0060a132b0fa191554dceda
Tommy Manyvanh <tommy_manyvanh@yahoo.com>
2020-02-04 15:36:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ExchangedCurrency from './Components/CurrencyConverter';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <>
+        <h2>Higher Order Component</h2>
+        <ExchangedCurrency/>
+      </>
     );
   }
 }
